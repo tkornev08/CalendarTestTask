c4ed39974d34797431457222f326fea397c91dc1
tkornev08 <92224870+tkornev08@users.noreply.github.com>
2023-10-13 21:22:33 +0300

    feature: set RU locale for moment.js

--- src/app/services/date.service.ts
+++ src/app/services/date.service.ts
@@ -1,12 +1,17 @@
 import {Injectable} from '@angular/core';
 import * as moment from "moment";
 import {BehaviorSubject} from "rxjs";
+import 'moment/locale/ru';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DateService {
   public date: BehaviorSubject<moment.Moment> = new BehaviorSubject(moment())
+
+  constructor() {
+    moment.locale('ru');
+  }
 
   changeMonth(dir: number) {
     const value = this.date.value.add(dir, 'month')
